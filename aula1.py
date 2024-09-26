109cc7c6116a129011d2712aa4d87311bd58dd25
Guilherme <spigoloniguilherme@gmail.com>
2024-09-25 21:32:35 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,10 @@
+print("Exercício: \n")
+
+print("1- O que é python? ")
+print("É uma linguagem de programação de alto nível que é baseada em inglês.")
+
+print("2- Qual programa (IDE) executa o arquivo.py? ")
+print("Vscode")
+
+print("3- Qual linguagem de programação o Python substituiu? ")
+print("Linguagem abc.")
