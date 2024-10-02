9110231a08381fdeff93a89710291a6c036fb3c4
Guilherme <spigoloniguilherme@gmail.com>
2024-10-02 20:12:11 -0300

    Add files via upload

---
+++ aula02_POO.py
@@ -0,0 +1,32 @@
+## Atividade 1
+#print("Faça a soma de núnmeros!")
+#numero1 = float(input("Primeiro número: ")) 
+#numero2 =float(input("Segundo número: "))
+#soma = numero1 + numero2
+#print(soma)
+
+
+
+## Atividade 2
+# primeiro_nome = input("Primeiro nome: ")
+# sobrenome = input("Sobrenome: ")
+# print("Nome completo: "+primeiro_nome + sobrenome)
+
+
+
+## Atividade 3
+# print("Calcule sua média!")
+# nota1 = float(input("Primeira nota: "))
+# nota2 = float(input("Segunda nota: "))
+# nota3 = float(input("Terceira nota: "))
+# media = (nota1 + nota2 + nota3) / 3
+# print(f"Sua média é: {media}")
+
+
+
+## Atividade 4
+# print("Calcule a área de um retângulo!")
+# base = float(input("Insira o valor da base: "))
+# altura = float(input("Insira o valor da altura: "))
+# area = base * altura
+# print(f"A área desse retângulo é: {area}")
