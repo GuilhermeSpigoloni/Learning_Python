d9f78f9161a6b6bab703967b8554df7db1afca80
Guilherme <spigoloniguilherme@gmail.com>
2024-10-04 20:04:51 -0300

    Add files via upload

---
+++ exercicio_04.py
@@ -0,0 +1,43 @@
+## Atividade 01
+
+# print("MÉDIA DE NÚMEROS!")
+# n1 = float(input("Digite o primero número: "))
+# n2 = float(input("Digite o segundo número: "))
+# n3 = float(input("Digite o terceiro número: "))
+# n4 = float(input("Digite o quarto número: "))
+# media = (n1 + n2 + n3 + n4) / 4
+# print(f"Sua média é {media}")
+
+
+## Atividade 02
+
+# print("CONVERSOR DE HORAS --> MINUTOS --> SEGUNDOS")
+# horas = float(input("Digite as horas: "))
+# minutos = horas * 60
+# segundos = minutos * 60
+# print(f"{horas} = {minutos} min\n{horas} = {segundos} seg")
+
+
+## Atividade 03
+
+# print("-------------------------------------------FORMULÁRIO-------------------------------------------")
+# nome = input("Digite seu primeiro nome: ")
+# sobrenome = input("Digite seu sobrenome: ")
+# idade = int(input("Digite sua idade: "))
+# nascimento = 2024 - idade
+# estado = input("Qual seu estado: ")
+# cidade = input("Qual sua cidade: ")
+# email = input("Informe seu e-mail: ")
+# telefone = input("Informe seu telefone para contato? ")
+# print("-------------------------------------------------------------")
+# print(f"Usuário: {nome} {sobrenome}")
+# print("-------------------------------------------------------------")
+# print(f"Nascido no ano de {nascimento}")
+# print("-------------------------------------------------------------")
+# print(f"Reside no estado {estado} na cidade {cidade}")
+# print("-------------------------------------------------------------")
+# print(f"Telefone: {telefone}")
+# print("-------------------------------------------------------------")
+# print(f"e-mail: {email}")
+# print("-------------------------------------------------------------")
+# print("Parabéns!! Seu cadastro foi concluído!!")
