83fca0dfae92c9fcc466a6a0ff4ac8f5e1b0aae7
Guilherme <spigoloniguilherme@gmail.com>
2024-10-16 19:41:39 -0300

    Add files via upload

---
+++ exercicio_05.py
@@ -0,0 +1,30 @@
+# Exercício 01
+
+idade = int(input("Informe sua idade: "))
+
+if idade < 18:
+    print("Você é menor de idade.")
+else:
+    print("Você já alcançou a maioridade.")
+
+
+# Exercício 02
+
+x = float(input("Primeiro número: "))
+y = float(input("Segundo número: "))
+
+if x == y:
+    print("Os números são iguais!!!")
+else:
+    print("Os números são diferentes.")
+
+
+# Exercício 04
+
+numero = float(input("Insira um número: "))
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é 0.")
