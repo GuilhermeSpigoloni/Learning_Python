b2491834c796e2343fa0cd012bee78e66dab103a
Guilherme <spigoloniguilherme@gmail.com>
2024-12-03 20:42:36 -0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,94 @@
+# l = [8,9,15]
+# for e in l:
+    # print(e)
+    # 
+# L = list(range(100,1100, 50))
+# print(L)
+# 
+# L = [1,7,2,4]
+# maximo = L[0]
+# for e in L:
+    # if e > maximo:
+        # if e > maximo:
+            # maximo = e
+# print(maximo)
+
+
+
+
+
+# l = [8,9,15]
+# for e in l:
+    # print(l)
+    # 
+# nome = ['G', 'u', 'i' , 'Gui']
+# for caracter in nome:
+    # print(caracter)
+    
+    
+    
+    
+   
+# 
+# for i in range(0,10):
+    # print(i + 1)
+# print('fim')
+
+
+
+
+
+# for i in range(10,-1, -1):
+    # print(f'Decolagem em: {i}')
+# print('DECOLAR!!!')
+
+
+
+
+
+# numero = int(input('Digite um número: '))
+# for i in range(0 , numero + 1):
+    # print(i)
+# print('fim')
+
+
+
+
+
+# inicio = int(input('Início: '))
+# fim = int(input('Fim: '))
+# for i in range(inicio, fim + 1):
+    # print(i)
+
+
+
+
+
+# inicio = int(input('Início: '))
+# fim = int(input('Fim: '))
+# passo = int(input('Passo: '))
+# 
+# for i in range(inicio, fim + 1, passo):
+    # print(i)
+
+
+
+# 
+# 
+# soma = 0
+# fim = int(input('Digite quantos números deseja somar: '))
+# for i in range(0 , fim):
+    # numero = int(input('Digite um valor: '))
+    # soma += numero
+# media = soma/fim
+# print(f'A soma dos números foi: {soma}, e a media foi {media:.2f}')
+
+
+
+
+# 
+# from time import sleep
+# for i in range(10, -1 ,-1):
+    # print(i)
+    # sleep(i)
+# print('Apitou, Começou!!')
