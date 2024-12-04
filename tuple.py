780f67dd1c614840efbb77cef032a2d8a6d0c9b4
Guilherme <spigoloniguilherme@gmail.com>
2024-12-03 21:41:17 -0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,26 @@
+# tupla = tuple()
+# tupla = ()
+# print(type(tuple))
+# print(dir(tuple))
+
+
+
+# tupla = ('Um') #string
+# print(type(tupla))
+# 
+# tupla = ('Um',) #tupla
+# print(type(tupla))
+
+
+
+# cores = ('Preto', 'Roxo', 'Verde', 'Azul', 'Branco')
+# 
+# print(cores[0])
+# print(cores[-1])
+# print(cores[:])
+# print(cores.index('Preto'))
+# print(cores.count('Preto'))
+# print(len(cores))
+
+
+
