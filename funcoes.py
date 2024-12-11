aa3bf10d001fcf19fbcc1fdb377b53b3035feaf1
Guilherme <spigoloniguilherme@gmail.com>
2024-12-10 21:51:08 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,71 @@
+# def soma(a,b):
+#     print(a+b)
+
+# soma(1,0)
+# soma(1,1)
+# soma(1,2)
+# soma(1,3)
+# soma(1,4)
+# soma(1,5)
+# soma(1,6)
+# soma(1,7)
+# soma(1,8)
+# soma(1,9)
+
+
+
+# def soma(a,b):
+#     return a + b
+
+# print(soma(1,2))
+
+
+
+# def epar(x):
+#     return x % 2 == 0
+
+# print(epar(12))
+# print(epar(13))
+
+
+
+# def par_impar(x):
+#     if x % 2 == 0:
+#         return f'O número {x} é Par'
+#     else:
+#         return f'O número {x} é Impar'
+
+# print(par_impar(10))
+# print(par_impar(11))
+
+
+
+
+
+
+#8.1
+# def maior_menor(a,b):
+#     if a >b:
+#         return a
+#     else:
+#         return b
+# print(maior_menor(5,6))
+# print(maior_menor(2,1))
+
+#8.2
+# a = int(input('Primeiro número: '))
+# b = int(input('Segundo número: '))
+# def multiplo(a,b):
+#     if a % b == 0:
+#         return f'O número {a} é divisível por {b}'
+#     else:
+#         return f'O número {a} não é divisível por {b}'
+
+#8.3
+# a = float(input('Valor do lado de um quadrado: '))
+# def area(a):
+#     return  a**2
+
+# print(f'A área desse quadrado é {area(a)}m quadrados')
+
+#8.4
